2bda699313e46aad42de079d7eabff230bd92290
rydbirkjr <arydbirk@gmail.com>
2014-04-13 19:23:57 -0400

    Updated with unsigned

--- MHash.cpp
+++ MHash.cpp
@@ -1,12 +1,17 @@
 #include "MHash.h"
 
 //Constructor
-MHash::MHash(int multiplier, int S, int B){
+MHash::MHash(uint multiplier, uint S, uint B){
     s = multiplier;
     p = S-log2(B);
 }
 
 //Hash function
-int MHash::hash(int key){
+uint MHash::hash(uint key){
     return (key * s) >> (w - p);
+}
+
+int main(){
+    MHash::MHash(1231232, 12312232,12322);
+    return 0;
 }
