395bf22919f00bcc915400bbed4baaec1463735b
Nicholas Ursa <nick.ursa@gmail.com>
2014-04-24 16:21:36 -0400

    Got simd workind.

--- play.cpp
+++ play.cpp
@@ -3,55 +3,16 @@
 #include<string>
 using namespace std;
 
-struct Brick {
-	int x;
-	int *y;
-};
-
-class Test {
-private:
-	int *a;  // this is an array
-	struct Brick *b;
-	int size;
-
-	static void log(string s){
-		cout << s <<  endl;	
-	}
-public:
-
-	Test(int size) {
-		this->size = size;
-		init();
-	}
-
-	void init(){
-		cerr << "Entering init\n";
-		this->a =new int[this->size];
-		this->b =new Brick[this->size];
-		for(int i = 0; i< size; i++){
-			cerr << "making brick" << i << "\n";
-			this->b[i].x=i;
-			this->b[i].y=new int[5];
-		}
-	}
-
-	~Test() {
-		int sz=10;
-		cerr << "Entering destructor\n";
-		for(int i = 0; i< 10; i++){
-			cerr << "deleting brick " << i << "\n";
-			delete[] this->b[i].y;
-		}
-		
-		delete this->b;
-		delete[] this->a;
-	}
-};
 
 // new comment 
-int main(int argv, char** argc){
+int main(int argc, char* argv[]){
 
-	Test *t = new Test();
-	delete t;
+	int i = stoi(argv[1]);
+
+	int a[i];
+	a[0]=5;
+
+	return 0;
+
 	
 }
--- simdst.c
+++ simdst.c
@@ -2,66 +2,127 @@
 #include <emmintrin.h>
 #include <pmmintrin.h>
 #include <tmmintrin.h>
-#include <smmintrin.h>
-#include <nmmintrin.h>
+/* #include <smmintrin.h> */
+/* #include <nmmintrin.h> */
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+
 /* #include <ammintrin.h>  AMD ONLY */
 
-struct MHash{
-    unsigned int s;
-    unsigned int p;
-};
+unsigned int*  __attribute__ ((aligned (16))) keys;
+unsigned int*  __attribute__ ((aligned (16))) pay;
 
-// we should look at makind this a linear 2D array
-unsigned int** keys;
-unsigned int** payload;
-struct MHash ** hashes;
+int B=4;  // number of entries per bucket
+int S=5;  // 2^S = max number of entries
+int Nm; // number of entries, for conventience
+int Nb;  // number of buckets
+int p;
+const unsigned int w = sizeof(int) * 8; // why ?
+int hashes[2] ;
 
-int N=0;
-int h=5;
-int B=16;
-int R=4;
-int S=16;
-int noOfBuckets;
-const unsigned int w = sizeof(int) * 8;
+// SIMD values which we only need to init once
+__m128i t; // number of tables
+__m128i hm; 
 
 void initialize_buckets(){
-	noOfBuckets=(1 << S) / B;  // << equive to 2^S
+    p=S - log2(B);
+    Nm=(1 << S); 
+    Nb = Nm / B;
 
-	keys=malloc(sizeof(unsigned int*) * S);
-	payload=malloc(sizeof(unsigned int*) * S);
+    // we will allocate a big block
+    posix_memalign((void **)&keys, 16, sizeof(unsigned int) * Nm);
+    posix_memalign((void **)&pay, 16, sizeof(unsigned int) * Nm);
 
-	int i=0;
-	for(i=0;i<S;i++){
-		keys[i]=calloc(sizeof(unsigned int),  B);	
-		payload[i]=calloc(sizeof(unsigned int), B);	
-	}
-
-	// initialize storage for hash parameters
-	hashes=malloc(sizeof(struct MHash *) * h);
-	for(i=0;i<h;i++){
-		hashes[i]=malloc(sizeof(struct MHash));
-	}
+    t=_mm_set_epi32(0, Nb, 0, Nb);
+    hm=_mm_set_epi32(0, hashes[1], 0, hashes[0]);
 }
 
-unsigned int hash(unsigned int key, struct MHash * hash){
-      return (key * (*hash).s) >> (w - (*hash).p);
+unsigned int hash(unsigned int key, unsigned int h){
+      return(key * h) >> (w - p);
 }
+
+/*
+ *
+ * B S h N
+ * H[0] H[1] ... H[h-1] K[0] P[0]
+ * K[1] P[1]
+ * ...
+ * K[2^S-1] P[2^S-1]
+ */
+void pv(__m128i to, char * s){
+    uint32_t * out =(uint32_t *) &to;
+    printf("%s: %u %u %u %u\n", s, out[3], out[2], out[1], out[0]);
+}
+
+/* example of load */
+
+/*
+ *
+ * char a[N], b[N], c[N];
+ * ...
+ * __m128i *av, *bv, *cv;
+ * av = (__m128i*)a;
+ * bv = (__m128i*)b;
+ * cv = (__m128i*)c;
+ * for (i = 0; i < N/16; i++)
+ * { __m128i br = _mm_loadu_si128(&bv[i];
+ *   __m128i cr = _mm_loadu_si128(&cv[i];
+ *     __m128i ar = _mm_add_epi8(br, cr);
+ *       _mm_storeu_si128(&av[i], ar);
+ *       }
+ *
+ */
+
+void dump_table(char* s){
+    int i,j;
+    printf("\n%s\n", s);
+    for(i=0; i<S; i++){
+        printf("%3d:\t",i);
+        for(j=0; j<B; j++){
+            printf(" %3d:%3d", keys[i*B + j], pay[i*B + j]); 
+        }
+        printf("\n");
+    }
+
+}
+
 
 unsigned int probe(unsigned int key){
 
-    unsigned int hashedValue;
-    unsigned int value = 0;
-    unsigned int count = 0;
+    // this stuff applies to both
+    __m128i k=_mm_set1_epi32(key);
+    __m128i hv= _mm_mul_epu32(hm, k);
+    __m128i hs= _mm_mul_epu32(hv, t);
 
-    for(int i = 0; i<h; i++){
-        hashedValue = hash(key,hashes[i]);
-        for(int k = 0; k<B; k++){
-            count += (keys[hashedValue][i] == key);
-            value += (((count == 2) & (keys[hashedValue][i] == key)) * payload[hashedValue][k]);
-        }
-    }
+    // now we need to extract the buckets
+    // cast it as array
+    uint32_t * hsa =(uint32_t *) &hs;
 
-    return value;
+    unsigned int a1=hsa[1]; // the bucket number for first hash
+    unsigned int a2=hsa[3]; // the bucket number for second hash
+     
+    a1 = a1 << 2;  // multiply by 4
+    a2 = a2 << 2;
+
+    __m128i k1 = _mm_load_si128((__m128i *) &(keys[0 + a1]));
+    __m128i mask1=_mm_cmpeq_epi32(k,k1);
+    __m128i p1 = _mm_load_si128((__m128i *) &(pay[0 + a1]));
+    __m128i v1=_mm_and_si128(p1,mask1);
+   
+    __m128i k2 = _mm_load_si128((__m128i *) &(keys[0 + a2]));
+    __m128i mask2=_mm_cmpeq_epi32(k,k2);
+    __m128i p2 = _mm_load_si128((__m128i *) &(pay[0 + a2]));
+    __m128i v2=_mm_and_si128(p2,mask2);
+
+    __m128i both=_mm_or_si128(v1, v2);
+
+    // I couldn't find or across will do manually
+    __m128i to=both;
+   
+    uint32_t * ex =(uint32_t *) &to;
+    unsigned int out = ex[0] | ex[1] | ex[2] | ex [3];  
+    return out;
 }
 
 // need to do this on initialization
@@ -69,6 +130,28 @@ unsigned int probe(unsigned int key){
 //    p = S-log2(B);
 
 int main(int argc, char* argv[]){
-	initialize_buckets();
-	return 0;
+    if(!strcmp(argv[1], "test")){
+        unsigned int h0=0xf918720c;
+        unsigned int h1=0x8f7201cc;
+        hashes[0]=h0;
+        hashes[1]=h1;
+        initialize_buckets();
+       
+        // let's insert some values, then probe
+        int bucket,i;
+        for(i=0;i<40;i++){
+            int v1=i;
+            bucket=hash(v1, h0);
+            int offset=(bucket * B) + (i % 4);
+            printf("i: %d, bucket %d, off: %d\n", i, bucket, offset);
+            keys[(bucket * B) + (i % 4)]=v1;
+            pay[(bucket * B) + (i % 4)]=v1 * 10;
+        }
+        dump_table("init");       
+        for(i=1;i<40;i++){
+            unsigned int out = probe(i);
+            printf("%d:%d\t", i,out);
+        }
+    }
+    return 0;
 }
